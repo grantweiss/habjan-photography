5f29f2e475e7dd7e77c1273e874ecdb1874cf613
Grant Weiss <grant.sweiss@gmail.com>
2017-06-21 12:43:18 -0500

    update home page and add photo categories

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -18,4 +18,75 @@ class WelcomeController < ApplicationController
     ]
   end
 
+  def catone
+    @gallery_photos = [
+      {image: "city.png",         thumbnail: "cityTn.png",         alt: "City Scape 1"},
+      {image: "city2.png",        thumbnail: "city2Tn.png",        alt: "City Scape 2"},
+      {image: "city3.png",        thumbnail: "city3Tn.png",        alt: "City Scape 3"},
+      {image: "city4.png",        thumbnail: "city4Tn.png",        alt: "City Scape 4"},
+      {image: "city5.png",        thumbnail: "city5Tn.png",        alt: "City Scape 5"},
+      {image: "birdsEyeCity.png", thumbnail: "birdsEyeCityTn.png", alt: "Birds Eye City"},
+      {image: "headShot.png",     thumbnail: "headShotTn.png",     alt: "Head Shot 1"},
+      {image: "headShot2.png",    thumbnail: "headShot2Tn.png",    alt: "Head Shot 2"},
+      {image: "headShot3.png",    thumbnail: "headShot3.png",      alt: "Head Shot 3"},
+      {image: "lookingDown.png",  thumbnail: "lookingDownTn.png",  alt: "Looking Down"},
+      {image: "sply50.png",       thumbnail: "sply50Tn.png",       alt: "Sply 350"},
+      {image: "brownNike.png",    thumbnail: "brownNikeTn.png",    alt: "Brown Nike"},
+      {image: "darkNike.png",     thumbnail: "darkNike.png",       alt: "Dark Nike"}
+    ]
+  end
+
+  def cattwo
+     @gallery_photos = [
+      {image: "city.png",         thumbnail: "cityTn.png",         alt: "City Scape 1"},
+      {image: "city2.png",        thumbnail: "city2Tn.png",        alt: "City Scape 2"},
+      {image: "city3.png",        thumbnail: "city3Tn.png",        alt: "City Scape 3"},
+      {image: "city4.png",        thumbnail: "city4Tn.png",        alt: "City Scape 4"},
+      {image: "city5.png",        thumbnail: "city5Tn.png",        alt: "City Scape 5"},
+      {image: "birdsEyeCity.png", thumbnail: "birdsEyeCityTn.png", alt: "Birds Eye City"},
+      {image: "headShot.png",     thumbnail: "headShotTn.png",     alt: "Head Shot 1"},
+      {image: "headShot2.png",    thumbnail: "headShot2Tn.png",    alt: "Head Shot 2"},
+      {image: "headShot3.png",    thumbnail: "headShot3.png",      alt: "Head Shot 3"},
+      {image: "lookingDown.png",  thumbnail: "lookingDownTn.png",  alt: "Looking Down"},
+      {image: "sply50.png",       thumbnail: "sply50Tn.png",       alt: "Sply 350"},
+      {image: "brownNike.png",    thumbnail: "brownNikeTn.png",    alt: "Brown Nike"},
+      {image: "darkNike.png",     thumbnail: "darkNike.png",       alt: "Dark Nike"}
+    ]
+  end
+
+  def catthree
+      @gallery_photos = [
+      {image: "city.png",         thumbnail: "cityTn.png",         alt: "City Scape 1"},
+      {image: "city2.png",        thumbnail: "city2Tn.png",        alt: "City Scape 2"},
+      {image: "city3.png",        thumbnail: "city3Tn.png",        alt: "City Scape 3"},
+      {image: "city4.png",        thumbnail: "city4Tn.png",        alt: "City Scape 4"},
+      {image: "city5.png",        thumbnail: "city5Tn.png",        alt: "City Scape 5"},
+      {image: "birdsEyeCity.png", thumbnail: "birdsEyeCityTn.png", alt: "Birds Eye City"},
+      {image: "headShot.png",     thumbnail: "headShotTn.png",     alt: "Head Shot 1"},
+      {image: "headShot2.png",    thumbnail: "headShot2Tn.png",    alt: "Head Shot 2"},
+      {image: "headShot3.png",    thumbnail: "headShot3.png",      alt: "Head Shot 3"},
+      {image: "lookingDown.png",  thumbnail: "lookingDownTn.png",  alt: "Looking Down"},
+      {image: "sply50.png",       thumbnail: "sply50Tn.png",       alt: "Sply 350"},
+      {image: "brownNike.png",    thumbnail: "brownNikeTn.png",    alt: "Brown Nike"},
+      {image: "darkNike.png",     thumbnail: "darkNike.png",       alt: "Dark Nike"}
+    ]
+  end
+
+  def catfour
+      @gallery_photos = [
+      {image: "city.png",         thumbnail: "cityTn.png",         alt: "City Scape 1"},
+      {image: "city2.png",        thumbnail: "city2Tn.png",        alt: "City Scape 2"},
+      {image: "city3.png",        thumbnail: "city3Tn.png",        alt: "City Scape 3"},
+      {image: "city4.png",        thumbnail: "city4Tn.png",        alt: "City Scape 4"},
+      {image: "city5.png",        thumbnail: "city5Tn.png",        alt: "City Scape 5"},
+      {image: "birdsEyeCity.png", thumbnail: "birdsEyeCityTn.png", alt: "Birds Eye City"},
+      {image: "headShot.png",     thumbnail: "headShotTn.png",     alt: "Head Shot 1"},
+      {image: "headShot2.png",    thumbnail: "headShot2Tn.png",    alt: "Head Shot 2"},
+      {image: "headShot3.png",    thumbnail: "headShot3.png",      alt: "Head Shot 3"},
+      {image: "lookingDown.png",  thumbnail: "lookingDownTn.png",  alt: "Looking Down"},
+      {image: "sply50.png",       thumbnail: "sply50Tn.png",       alt: "Sply 350"},
+      {image: "brownNike.png",    thumbnail: "brownNikeTn.png",    alt: "Brown Nike"},
+      {image: "darkNike.png",     thumbnail: "darkNike.png",       alt: "Dark Nike"}
+    ]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get 'catone' => 'welcome#catone'
+  get 'cattwo' => 'welcome#cattwo'
+  get 'catthree' => 'welcome#catthree'
+  get 'catfour' => 'welcome#catfour'
+
 
   get 'contact-me', to: 'messages#new', as: 'new_message'
   post 'contact-me', to: 'messages#create', as: 'create_message'
