1d6a5a63b353490c8cfb141280a2dcfd8c05dd14
Grant <grant.sweiss@gmail.com>
2017-06-24 11:38:56 -0500

    add new photo content

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,56 +1,27 @@
 class WelcomeController < ApplicationController
 
   def index
-    @gallery_photos = [
-      {image: "city.png",         thumbnail: "cityTn.png",         alt: "City Scape 1"},
-      {image: "city2.png",        thumbnail: "city2Tn.png",        alt: "City Scape 2"},
-      {image: "city3.png",        thumbnail: "city3Tn.png",        alt: "City Scape 3"},
-      {image: "city4.png",        thumbnail: "city4Tn.png",        alt: "City Scape 4"},
-      {image: "city5.png",        thumbnail: "city5Tn.png",        alt: "City Scape 5"},
-      {image: "birdsEyeCity.png", thumbnail: "birdsEyeCityTn.png", alt: "Birds Eye City"},
-      {image: "headShot.png",     thumbnail: "headShotTn.png",     alt: "Head Shot 1"},
-      {image: "headShot2.png",    thumbnail: "headShot2Tn.png",    alt: "Head Shot 2"},
-      {image: "headShot3.png",    thumbnail: "headShot3.png",      alt: "Head Shot 3"},
-      {image: "lookingDown.png",  thumbnail: "lookingDownTn.png",  alt: "Looking Down"},
-      {image: "sply50.png",       thumbnail: "sply50Tn.png",       alt: "Sply 350"},
-      {image: "brownNike.png",    thumbnail: "brownNikeTn.png",    alt: "Brown Nike"},
-      {image: "darkNike.png",     thumbnail: "darkNike.png",       alt: "Dark Nike"}
-    ]
   end
 
   def catone
     @gallery_photos = [
-      {image: "city.png",         thumbnail: "cityTn.png",         alt: "City Scape 1"},
-      {image: "city2.png",        thumbnail: "city2Tn.png",        alt: "City Scape 2"},
-      {image: "city3.png",        thumbnail: "city3Tn.png",        alt: "City Scape 3"},
-      {image: "city4.png",        thumbnail: "city4Tn.png",        alt: "City Scape 4"},
-      {image: "city5.png",        thumbnail: "city5Tn.png",        alt: "City Scape 5"},
-      {image: "birdsEyeCity.png", thumbnail: "birdsEyeCityTn.png", alt: "Birds Eye City"},
-      {image: "headShot.png",     thumbnail: "headShotTn.png",     alt: "Head Shot 1"},
-      {image: "headShot2.png",    thumbnail: "headShot2Tn.png",    alt: "Head Shot 2"},
-      {image: "headShot3.png",    thumbnail: "headShot3.png",      alt: "Head Shot 3"},
-      {image: "lookingDown.png",  thumbnail: "lookingDownTn.png",  alt: "Looking Down"},
-      {image: "sply50.png",       thumbnail: "sply50Tn.png",       alt: "Sply 350"},
-      {image: "brownNike.png",    thumbnail: "brownNikeTn.png",    alt: "Brown Nike"},
-      {image: "darkNike.png",     thumbnail: "darkNike.png",       alt: "Dark Nike"}
+      {image: "home_1.png",        thumbnail: "home_1Tn.png",         alt: "City Scape 1"},
+      {image: "home_2.png",        thumbnail: "home_2Tn.png",        alt: "City Scape 2"},
+      {image: "home_3.png",        thumbnail: "home_3Tn.png",        alt: "City Scape 3"},
+      {image: "home_4.png",        thumbnail: "home_4Tn.png",        alt: "City Scape 4"},
+      {image: "home_5.png",        thumbnail: "home_5Tn.png",        alt: "City Scape 5"}
     ]
   end
 
   def cattwo
      @gallery_photos = [
-      {image: "city.png",         thumbnail: "cityTn.png",         alt: "City Scape 1"},
-      {image: "city2.png",        thumbnail: "city2Tn.png",        alt: "City Scape 2"},
-      {image: "city3.png",        thumbnail: "city3Tn.png",        alt: "City Scape 3"},
-      {image: "city4.png",        thumbnail: "city4Tn.png",        alt: "City Scape 4"},
-      {image: "city5.png",        thumbnail: "city5Tn.png",        alt: "City Scape 5"},
-      {image: "birdsEyeCity.png", thumbnail: "birdsEyeCityTn.png", alt: "Birds Eye City"},
-      {image: "headShot.png",     thumbnail: "headShotTn.png",     alt: "Head Shot 1"},
-      {image: "headShot2.png",    thumbnail: "headShot2Tn.png",    alt: "Head Shot 2"},
-      {image: "headShot3.png",    thumbnail: "headShot3.png",      alt: "Head Shot 3"},
-      {image: "lookingDown.png",  thumbnail: "lookingDownTn.png",  alt: "Looking Down"},
-      {image: "sply50.png",       thumbnail: "sply50Tn.png",       alt: "Sply 350"},
-      {image: "brownNike.png",    thumbnail: "brownNikeTn.png",    alt: "Brown Nike"},
-      {image: "darkNike.png",     thumbnail: "darkNike.png",       alt: "Dark Nike"}
+      {image: "people_1.png",      thumbnail: "people_1Tn.png",      alt: "Head Shot 1"},
+      {image: "people_2.png",      thumbnail: "people_2Tn.png",      alt: "Head Shot 2"},
+      {image: "people_3.png",      thumbnail: "people_3Tn.png",      alt: "Head Shot 3"},
+      {image: "people_4.png",      thumbnail: "people_4Tn.png",      alt: "Head Shot 4"},
+      {image: "people_5.png",      thumbnail: "people_5Tn.png",      alt: "Head Shot 5"},
+      {image: "people_6.png",      thumbnail: "people_6Tn.png",      alt: "Head Shot 6"},
+      {image: "headShot.png",      thumbnail: "headShotTn.png",      alt: "Head Shot 7"}
     ]
   end
 
