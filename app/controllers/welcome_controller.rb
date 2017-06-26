ea71a1916ba0c631242048e7ae774561cb9b450e
Grant Weiss <grant.sweiss@gmail.com>
2017-06-26 11:57:14 -0500

    update Things and Travel with thumbnails

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -27,25 +27,25 @@ class WelcomeController < ApplicationController
 
   def catthree
       @gallery_photos = [
-      {image: "city.png",         thumbnail: "cityTn.png",         alt: "City Scape 1"},
-      {image: "city2.png",        thumbnail: "city2Tn.png",        alt: "City Scape 2"},
-      {image: "city3.png",        thumbnail: "city3Tn.png",        alt: "City Scape 3"},
-      {image: "city4.png",        thumbnail: "city4Tn.png",        alt: "City Scape 4"},
-      {image: "city5.png",        thumbnail: "city5Tn.png",        alt: "City Scape 5"},
-      {image: "birdsEyeCity.png", thumbnail: "birdsEyeCityTn.png", alt: "Birds Eye City"},
-      {image: "headShot.png",     thumbnail: "headShotTn.png",     alt: "Head Shot 1"},
-      {image: "headShot2.png",    thumbnail: "headShot2Tn.png",    alt: "Head Shot 2"}
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"}
     ]
   end
 
   def catfour
       @gallery_photos = [
-      {image: "city.png",         thumbnail: "cityTn.png",         alt: "City Scape 1"},
-      {image: "city2.png",        thumbnail: "city2Tn.png",        alt: "City Scape 2"},
-      {image: "city3.png",        thumbnail: "city3Tn.png",        alt: "City Scape 3"},
-      {image: "city4.png",        thumbnail: "city4Tn.png",        alt: "City Scape 4"},
-      {image: "city5.png",        thumbnail: "city5Tn.png",        alt: "City Scape 5"},
-      {image: "birdsEyeCity.png", thumbnail: "birdsEyeCityTn.png", alt: "Birds Eye City"}
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"},
+      {image: "temp.png",         thumbnail: "temp.png",           alt: "Temp"}
     ]
   end
 end
