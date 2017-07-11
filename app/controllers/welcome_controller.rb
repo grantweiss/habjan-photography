e26ffe0c7a139e26538757548e586667d8a7df82
Grant Weiss <grant.sweiss@gmail.com>
2017-07-11 14:12:26 -0500

    upload images to cloud

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,23 +5,23 @@ class WelcomeController < ApplicationController
 
   def catone
     @gallery_photos = [
-      {image: "home_1.png",        thumbnail: "home_1Tn.png",         alt: "City Scape 1"},
-      {image: "home_2.png",        thumbnail: "home_2Tn.png",        alt: "City Scape 2"},
-      {image: "home_3.png",        thumbnail: "home_3Tn.png",        alt: "City Scape 3"},
-      {image: "home_4.png",        thumbnail: "home_4Tn.png",        alt: "City Scape 4"},
-      {image: "home_5.png",        thumbnail: "home_5Tn.png",        alt: "City Scape 5"}
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/home_1.png",        thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/home_1Tn.png",         alt: "City Scape 1"},
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/home_2.png",        thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/home_2Tn.png",        alt: "City Scape 2"},
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/home_3.png",        thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/home_3Tn.png",        alt: "City Scape 3"},
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/home_4.png",        thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/home_4Tn.png",        alt: "City Scape 4"},
+      {image: "home_5.png",        thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/home_5Tn.png",        alt: "City Scape 5"}
     ]
   end
 
   def cattwo
      @gallery_photos = [
-      {image: "people_1.png",      thumbnail: "people_1Tn.png",      alt: "Head Shot 1"},
-      {image: "people_2.png",      thumbnail: "people_2Tn.png",      alt: "Head Shot 2"},
-      {image: "people_3.png",      thumbnail: "people_3Tn.png",      alt: "Head Shot 3"},
-      {image: "people_4.png",      thumbnail: "people_4Tn.png",      alt: "Head Shot 4"},
-      {image: "people_5.png",      thumbnail: "people_5Tn.png",      alt: "Head Shot 5"},
-      {image: "people_6.png",      thumbnail: "people_6Tn.png",      alt: "Head Shot 6"},
-      {image: "headShot.png",      thumbnail: "headShotTn.png",      alt: "Head Shot 7"}
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_1.png",      thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_1Tn.png",      alt: "Head Shot 1"},
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_2.png",      thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_2Tn.png",      alt: "Head Shot 2"},
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_3.png",      thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_3Tn.png",      alt: "Head Shot 3"},
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_4.png",      thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_4Tn.png",      alt: "Head Shot 4"},
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_5.png",      thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_5Tn.png",      alt: "Head Shot 5"},
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_6.png",      thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/people_6Tn.png",      alt: "Head Shot 6"},
+      {image: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/headShot.png",      thumbnail: "https://s3.us-east-2.amazonaws.com/habjan-photography-images/headShotTn.png",      alt: "Head Shot 7"}
     ]
   end
 
