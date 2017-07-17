c15c04a1cbf722fad6ef7e572c5fb61bc5df2a5d
Justin White <Jjustinwhite@mac.com>
2017-07-17 14:56:12 -0500

    basic user login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'catone' => 'welcome#catone'
   get 'cattwo' => 'welcome#cattwo'
   get 'catthree' => 'welcome#catthree'
   get 'catfour' => 'welcome#catfour'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
 
   get 'contact-me', to: 'messages#new', as: 'new_message'
